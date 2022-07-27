807fac35eb09f535608cf0a0b9d6a3a8fde38aca
daniarfadd <daniarfadaffa@yahoo.com>
2022-07-27 16:55:12 +0700

    render question and answer from api

--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -1,48 +1,91 @@
 import { Box } from "@mui/system"
-import { Typography, Button } from "@mui/material"
+import { Typography, Button, CircularProgress } from "@mui/material"
 import useAxios from "../hooks/useAxios"
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+
+
+  // random number to randomize the answers
+  const getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max))
+  }
 
 function Questions() {
 
-  // question_category: "", 
-  // question_difficulty: "", 
-  // question_type: "",
-  // amount_of_question: 10,
-
   const question = useSelector(state => state.question.value)
   // console.log(question)
-  let apiUrl = `/api.php?amount=${question.amount_of_question}`
 
-  if(question.question_category){
-    apiUrl = apiUrl.concat(`&category=${question.question_category}`)
-  }
-  if(question.question_difficulty){
-    apiUrl = apiUrl.concat(`&difficulty=${question.question_difficulty}`)
-  }
-  if(question.question_type){
-    apiUrl = apiUrl.concat(`&type=${question.question_type}`)
-  }
 
-  // console.log(apiUrl)
+    let apiUrl = `/api.php?amount=${question.amount_of_question}`
+
+    if(question.question_category){
+      apiUrl = apiUrl.concat(`&category=${question.question_category}`)
+    }
+    if(question.question_difficulty){
+      apiUrl = apiUrl.concat(`&difficulty=${question.question_difficulty}`)
+    }
+    if(question.question_type){
+      apiUrl = apiUrl.concat(`&type=${question.question_type}`)
+    }
 
 
   const { response, error, loading } = useAxios({ url: apiUrl })
 
-  // console.log(response)
+  console.log(response)
+
+  // console.log(apiUrl)
+
+
+  const [questionIndex, setQuestionIndex] = useState(0)
+  const [options, setOptions] = useState([])
+  
+  console.log(options)
+
+  useEffect(() => {
+
+    if(response?.results.length){
+      const question = response.results[questionIndex]
+      let answers = [...question.incorrect_answers];
+      answers.splice(getRandomInt(question.incorrect_answers.length),
+      0,
+      question.correct_answer
+      );
+      setOptions(answers)
+    }
+
+  }, [response])
+
+
+  // Map the answers of the question
+  const mappedAnswers = options.map( (answer, index) => (
+    <Box mt={2}>
+        <Button variant="outlined" key={index} >{answer}</Button>
+      </Box>
+  ))
+
+
+  if(loading) {
+    return (
+        <Box mt={20}>
+            <CircularProgress color="secondary" />
+        </Box>
+    )
+}
+
+if(error) {
+  return (
+      <Typography variant="h4" mt={20} color="red">
+          Something Went Wrong!
+      </Typography>
+  )
+}
 
   return (
     <Box>
-      <Typography variant="h4" >Question 1</Typography>
-      <Typography mt={5} >This is the question :D</Typography>
+      <Typography variant="h4" >Question {questionIndex + 1}</Typography>
+      <Typography mt={5} >{response.results[questionIndex].question}</Typography>
 
-      <Box mt={2}>
-        <Button variant="outlined" >Answer 1</Button>
-      </Box>
-
-      <Box mt={2}>
-        <Button variant="outlined" >Answer 2</Button>
-      </Box>
+      {mappedAnswers}
 
       <Box mt={5}>
         Score: 2 / 6
